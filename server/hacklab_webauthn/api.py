449e53c1af56eea5a922514a839d3b326a333dbc
Marek Skrajnowski <marek@codequest.com>
2019-11-21 23:37:21 +0100

    feat(server): added marshmallow and cors

--- server/hacklab_webauthn/api.py
+++ server/hacklab_webauthn/api.py
@@ -1,15 +1,21 @@
 from flask import Flask, Blueprint
+from flask_cors import CORS
 from flask_praetorian import Praetorian
+from flask_marshmallow import Marshmallow, fields
 
 from .models import User
 
 
+cors = CORS()
 guard = Praetorian()
+ma = Marshmallow()
 bp = Blueprint("api", __name__)
 
 
 def init_app(app: Flask, url_prefix=""):
+    cors.init_app(app)
     guard.init_app(app, user_class=User)
+    ma.init_app(app)
     app.register_blueprint(bp, url_prefix=url_prefix)
 
 
